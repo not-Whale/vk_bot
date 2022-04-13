a81a9a73f9ab55dcd59844c579b3bc4c783661ee
Rezepin Nikita <rezepinn@yandex.ru>
2022-04-13 23:43:52 +0300

    Инициализирован парсер аргументов командной строки

--- src/main/vk_bot.py
+++ src/main/vk_bot.py
@@ -1,7 +1,58 @@
 import argparse
+import sys
 from src.main.bot.adrenaline_bot import Adrenaline_bot
 
 
+def init_parser():
+    parser = argparse.ArgumentParser(
+        prog='vk_bot',
+        description='vk_bot - bot for vk community',
+        epilog='recommended usage: vk_bot --logging'
+    )
+    parser.add_argument(
+        '-v', '--version',
+        action='version',
+        version='%(prog)s v0.2.0',
+    )
+    parser.add_argument(
+        '-l', '--logging',
+        action='store_true',
+        help='print logging messages while working'
+    )
+    parser.add_argument(
+        '-c', '--clean_start',
+        action='store_true',
+        help='delete admins info saved before'
+    )
+    parser.add_argument(
+        '-f', '--first_start',
+        action='store_true',
+        help='delete all info saved before (except for config file)'
+    )
+    parser.add_argument(
+        '--config_file',
+        default='/bot/conf.py',
+        help='path to config file (default: "/bot/conf.py")',
+        metavar='path'
+    )
+    parser.add_argument(
+        '--admins_list',
+        help='path to file with admins needed to add list',
+        metavar='path'
+    )
+    parser.add_argument(
+        '--auth_type',
+        choices=['community', 'user'],
+        default='community',
+        help='authentication type (choose from: "community", "user"; default: "community")',
+        metavar='type'
+    )
+    return parser
+
+
 if __name__ == '__main__':
-    bot = Adrenaline_bot()
-    bot.start_bot()
+    args_parser = init_parser()
+    args_parser.print_help()
+    # args = args_parser.parse_args(sys.argv[1:])
+    # bot = Adrenaline_bot()
+    # bot.start_bot()

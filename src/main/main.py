47e0117e8c8764a35eef25a89611dd05b3a846e5
Rezepin Nikita <rezepinn@yandex.ru>
2022-04-13 21:22:05 +0300

    Переменовал файл запуска, создал отслеживаемый файл с индивидуальными константами

--- src/main/main.py
+++
@@ -1,9 +0,0 @@
-from src.main.bot.adrenaline_bot import Adrenaline_bot
-# from src.main.user.admin import Admin
-
-# LOPATA_ID = '448223022'
-# DK_ID = '427635051'
-# POLAND_ID = ''
-
-bot = Adrenaline_bot()
-bot.start_bot()
---
+++ src/main/vk_bot.py
@@ -0,0 +1,7 @@
+import argparse
+from src.main.bot.adrenaline_bot import Adrenaline_bot
+
+
+if __name__ == '__main__':
+    bot = Adrenaline_bot()
+    bot.start_bot()
